1020d54e9d82018964f7e1830533acb6d27fb9ee
Carlos Alberto <carlos.ruiz@softwareradiosystems.com>
2021-06-25 20:58:49 +0200

    fix

--- generate_doc.py
+++ generate_doc.py
@@ -64,7 +64,7 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 ################################################################################
 if (OUTPUT_TYPE == 'html'):
     html_index = '<h1>Open source projects:</h1>\n\n<ul>\n'
-    html_index += f"Created: {dt_string}\n"
+    html_index += f"Created: {dt_string}\n\n"
     repositories = read_repositories(CONFIG_PATH)
     for rep in repositories:
         name = rep
