5b8c8da9f3f18eec184fbf059f09123a00b4d3ca
Carlos Alberto <carlos.ruiz@softwareradiosystems.com>
2021-06-25 20:08:31 +0200

    fix bugs

--- generate_doc.py
+++ generate_doc.py
@@ -70,7 +70,7 @@ if (OUTPUT_TYPE == 'html'):
         vhdl_symbol = repository['vhdl_symbol']
         verilog_symbol = repository['verilog_symbol']
 
-        html_index += f"    <li>Project: <a href=\"{name}_doc/index.html\">{name}</a></li>\n"
+        html_index += f"    <li>Project: <a href=\"{name}_doc/index.html\">{name}</a> (<a href=\"{url}\">go to Github project</a>) </li>\n"
         git_clone(url)
         execute_teroshdl(name, folder, OUTPUT_TYPE, vhdl_symbol, verilog_symbol)
     html_index += '</ul>\n'
@@ -92,7 +92,7 @@ if (OUTPUT_TYPE == 'markdown'):
         vhdl_symbol = repository['vhdl_symbol']
         verilog_symbol = repository['verilog_symbol']
 
-        html_index += f"Project: [${name} ](./{name}_doc/README.md)\n"
+        html_index += f"Project: [{name} ](./{name}_doc/README.md) ([go to Github project]({url}))\n"
         git_clone(url)
         execute_teroshdl(name, folder, OUTPUT_TYPE, vhdl_symbol, verilog_symbol)
 
